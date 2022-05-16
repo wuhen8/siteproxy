0e5ab91934dcc34ab45e892411622f9b957eb371
wuhen8 <93480904+wuhen8@users.noreply.github.com>
2022-05-16 03:08:55 -0400

    d

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'dlp.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
